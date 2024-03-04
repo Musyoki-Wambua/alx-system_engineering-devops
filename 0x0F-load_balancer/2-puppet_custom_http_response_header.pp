16067479094962a9aa9dd6e1c973924cf70cca35
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2024-03-04 16:54:26 +0300

    Shellcheck error

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,12 +1,12 @@
 # Installs a Nginx server with custome HTTP header
 
-exec {'update':
+exec { 'update':
   provider => shell,
   command  => 'sudo apt-get -y update',
   before   => Exec['install Nginx'],
 }
 
-exec {'install Nginx':
+exec { 'install Nginx':
   provider => shell,
   command  => 'sudo apt-get -y install nginx',
   before   => Exec['add_header'],
